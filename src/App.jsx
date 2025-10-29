986722a08bb01d2a90e035fb0925f27501823ba5
[mid9coward] <[hieuvu0422@gmail.com]>
2025-10-29 10:10:59 +0700

    implement model switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import ProductViewer from "./components/ProductViewer";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
